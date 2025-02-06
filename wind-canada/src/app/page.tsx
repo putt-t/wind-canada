d53ba8f0908d7b5d1f0fa095923c14c09e8932d5
putt-t <putt.t@outlook.com>
2025-02-06 01:22:41 -0800

    Added repo link

--- wind-canada/src/app/page.tsx
+++ wind-canada/src/app/page.tsx
@@ -11,11 +11,20 @@ export default function Home() {
       </header>
 
       <main className="flex-grow max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col">
-        <section className="text-center">
+        <section>
           <h1 className="text-2xl sm:text-3xl font-bold mb-2 text-gray-800">
             currently under development.
           </h1>
-          <p className="text-gray-600 mt-5 mb-5">
+          <span className="text-gray-600">repo link - </span>
+          <a
+            href="https://github.com/putt-t/wind-canada"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline text-blue-300"
+          >
+            https://github.com/putt-t/wind-canada
+          </a>{" "}
+          <p className="text-gray-600 mt-3 mb-5">
             a web-based tool for calculating wind loads on buildings, compliant
             with the National Building Code of Canada 2020.
           </p>
